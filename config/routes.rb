f9ba37b9594531cdc327b1ea7cb23d22465155d7
Joshua Carver <jcarver989@gmail.com>
2010-02-23 20:25:12 -0800

    Layed out basic html framework for homepage - need to import images from photoshop

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # Sample resource route with sub-resources:
   #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
+
   # Sample resource route with more complex sub-resources
   #   map.resources :products do |products|
   #     products.resources :comments
@@ -32,6 +32,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.root :controller => 'home'
 
   # See how all your routes lay out with "rake routes"
 
@@ -41,3 +42,4 @@ ActionController::Routing::Routes.draw do |map|
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
+
